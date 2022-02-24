b6af01274208671aacc545d49a6f80b1b59bba8f
magdamargineanu <margineanu.magdalena@yahoo.com>
2022-02-24 15:53:29 +0200

    first commit

---
+++ src/main/java/mmir2764/Main.java
@@ -0,0 +1,7 @@
+package mmir2764;
+
+public class Main {
+    public static void main(String[] args) {
+        mmir2764.MainFX.main(args);
+    }
+}
---
+++ src/main/java/mmir2764/model/PaymentType.java
@@ -0,0 +1,5 @@
+package mmir2764.model;
+
+public enum PaymentType {
+    Cash, Card
+}
