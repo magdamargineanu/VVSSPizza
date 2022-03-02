1c5a786f085f3f024e7ad6bd4d447335e224795d
magdamargineanu <margineanu.magdalena@yahoo.com>
2022-03-02 16:35:48 +0200

    code improvements

--- src/main/java/mmir2764/repository/PaymentRepository.java
+++ src/main/java/mmir2764/repository/PaymentRepository.java
@@ -1,15 +1,14 @@
 package mmir2764.repository;
 
 import mmir2764.model.Payment;
-import mmir2764.model.PaymentType;
 import org.apache.log4j.Logger;
 
-import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class PaymentRepository {
     private static String filename = "src/main/resources/data/payments.txt";
@@ -17,26 +16,26 @@ public class PaymentRepository {
 
     private final Logger logger = Logger.getLogger(this.getClass());
 
-    public PaymentRepository(){
+    public PaymentRepository() {
         this.paymentList = new ArrayList<>();
     }
 
-    public void add(Payment payment){
+    public void add(Payment payment) {
         paymentList.add(payment);
         writeAll();
     }
 
-    public List<Payment> getAll(){
+    public List<Payment> getAll() {
         return paymentList;
     }
 
-    public void writeAll(){
+    public void writeAll() {
 
         ClassLoader classLoader = PaymentRepository.class.getClassLoader();
         File file = new File(filename);
 
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file,true))) {
-            for (Payment payment:paymentList) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+            for (Payment payment : paymentList) {
                 logger.info(payment.toString());
                 bw.write(payment.toString());
                 bw.newLine();
